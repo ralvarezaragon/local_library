d659ed2615493982b13b008cdf5a5b53cf28e4dc
ralvarezaragon <rafael.alvarez@basebone.com>
2017-06-20 12:04:49 +0200

    credentiasl added

--- bash/import_files_loop.sh
+++ bash/import_files_loop.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#./import_files_loop.sh -h 10.0.3.1 -d esme_mesh_tracking -f output_in -s 2
+#./import_files_loop.sh -h 10.0.3.7 -d esme_mesh_tracking -f output_in -s 5
 while getopts h:d:f:s: opts; do
   case ${opts} in
     h)
@@ -15,8 +15,7 @@ done
 
 for file in /tmp/${folder}/*.sql; do
   echo "Importing ${file} ..."
-  time mysql -h ${host} ${dbname} < $file  
-  mv "${file}" "${file}.imp"
+  time mysql -h ${host} -u admin -pwastinglightdearrosemary ${dbname} < $file  &&  mv "${file}" "${file}.imp"
   sleep ${sleep}s;
   echo "done!"
 done  
