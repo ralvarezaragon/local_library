7977b763da64e6cf92a21d282d887cd557564d89
ralvarezaragon <rafael.alvarez@basebone.com>
2017-06-20 11:58:53 +0200

    mysql import enabled

--- bash/import_files_loop.sh
+++ bash/import_files_loop.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#./import_files_loop.sh -h 10.0.3.1 -d users_ralvarez -f output_test -s 2
+#./import_files_loop.sh -h 10.0.3.1 -d esme_mesh_tracking -f output_in -s 2
 while getopts h:d:f:s: opts; do
   case ${opts} in
     h)
@@ -15,9 +15,8 @@ done
 
 for file in /tmp/${folder}/*.sql; do
   echo "Importing ${file} ..."
-  #time mysql -h ${host} ${dbname} < $file
-  echo "mysqli -h ${host} ${dbname} < $file"
-  #rm $file
+  time mysql -h ${host} ${dbname} < $file  
+  mv "${file}" "${file}.imp"
   sleep ${sleep}s;
   echo "done!"
 done  
