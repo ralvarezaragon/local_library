0586d8ddf62316d7c96b26ccdd7d011087c702ab
ralvarezaragon <rafael.alvarez@basebone.com>
2017-07-26 09:35:01 +0200

    regular dev commit

--- python/prom_exporters/query_performance.py
+++ python/prom_exporters/query_performance.py
@@ -67,7 +67,7 @@ if __name__ == '__main__':
       #dict_metric['count'] = result['c'],
       #dict_metric['time'] = result['av']
       print("=================== {0} ========================".format(node))
-      print output.split('\n')
+      print result.split('\n')
     #get_metric(conn["host"], result, "count", g_count)
     #get_metric(conn["host"], result, "time", g_time)		
     #print "{0}::mysql_query_count and time running".format(time)
