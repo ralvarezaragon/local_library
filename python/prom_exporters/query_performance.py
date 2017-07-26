3e59c174b6db68a061a8136e00246c8140272387
ralvarezaragon <rafael.alvarez@basebone.com>
2017-07-26 09:55:56 +0200

    regular dev commit

--- python/prom_exporters/query_performance.py
+++ python/prom_exporters/query_performance.py
@@ -50,7 +50,8 @@ if __name__ == '__main__':
     '10.0.3.41',
     '10.0.3.42'
   ]
-  dict_metric = {'node': '', 'count': '', 'time': ''}
+  dict_metric = {}
+  l_metric = []
   # Start up the server to expose the metrics.
   start_http_server(8003)	
   # build the metrics
@@ -67,9 +68,11 @@ if __name__ == '__main__':
       result_list = result.split('\n')
       del result_list[-1]
       result_list = map(int, result_list)
-      dict_metric['node'].update(node)
-      dict_metric['count'].update(result_list)
-      dict_metric['time'].update(round(sum(result_list) / float(len(result_list)), 2))
+      l_metric.extend = [
+        dict_metric['node'] = node,
+        dict_metric['count'] = result_list,
+        dict_metric['time'] = round(sum(result_list) / float(len(result_list)), 2)
+      ]
     #get_metric(conn["host"], result, "count", g_count)
     #get_metric(conn["host"], result, "time", g_time)		
     #print "{0}::mysql_query_count and time running".format(time)
