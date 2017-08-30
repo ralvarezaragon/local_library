817e0a15ee7b1709d9a4a91283c7640df2df5bb7
ralvarezaragon <rafael.alvarez@basebone.com>
2017-08-30 12:14:19 +0200

    dev updates

--- python/prom_exporters/profile_exporter.py
+++ python/prom_exporters/profile_exporter.py
@@ -7,7 +7,7 @@ import re
 p = sub.Popen(('sudo', 'tcpdump', '-i', 'eno2', '-s', '0', '-l', '-w', '-', 'dst', 'port 3306'), stdout=sub.PIPE)
 for row in iter(p.stdout.readline, b''):  
   if row.find('INSERT') > -1:
-    query_obj = re.search('(?<=INSERT INTO `)([a-z_0-9]*)`.`([a-z_0-9]*)', query)
+    query_obj = re.search('(?<=INSERT INTO `)([a-z_0-9]*)`.`([a-z_0-9]*)', row)
     query['dbname'] = dbname_obj.group(0)
     query['tname'] = dbname_obj.group(1)
     query['type'] = 'INSERT'
