f093d60f5ecf78ed777a338808c630c9d8ed0548
ralvarezaragon <rafael.alvarez@basebone.com>
2017-08-30 10:57:16 +0200

    dev updates

--- python/prom_exporters/profile_exporter.py
+++ python/prom_exporters/profile_exporter.py
@@ -2,6 +2,6 @@
 
 import subprocess as sub
 
-p = sub.Popen(('sudo', 'tcpdump -i eno2 -s 0 -l -w - dst port 3306', '-l'), stdout=sub.PIPE)
+p = sub.Popen(('sudo', 'tcpdump', '-l'), stdout=sub.PIPE)
 for row in iter(p.stdout.readline, b''):
     print row.rstrip()   # process here
