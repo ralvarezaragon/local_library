c809e0291244dfd8ca602cf6ad58a273e2d60185
ralvarezaragon <rafael.alvarez@basebone.com>
2017-08-30 12:14:39 +0200

    dev updates

--- python/prom_exporters/profile_exporter.py
+++ python/prom_exporters/profile_exporter.py
@@ -8,8 +8,8 @@ p = sub.Popen(('sudo', 'tcpdump', '-i', 'eno2', '-s', '0', '-l', '-w', '-', 'dst
 for row in iter(p.stdout.readline, b''):  
   if row.find('INSERT') > -1:
     query_obj = re.search('(?<=INSERT INTO `)([a-z_0-9]*)`.`([a-z_0-9]*)', row)
-    query['dbname'] = dbname_obj.group(0)
-    query['tname'] = dbname_obj.group(1)
+    query['dbname'] = query_obj.group(0)
+    query['tname'] = query_obj.group(1)
     query['type'] = 'INSERT'
     print query
   else:
