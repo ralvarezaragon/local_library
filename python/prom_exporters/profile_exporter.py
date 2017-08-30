08d2f7f082bbf72ae053cf0e7610ee2ea7fad27e
ralvarezaragon <rafael.alvarez@basebone.com>
2017-08-30 12:15:28 +0200

    dev updates

--- python/prom_exporters/profile_exporter.py
+++ python/prom_exporters/profile_exporter.py
@@ -3,13 +3,13 @@
 import subprocess as sub
 import re
 
-
+query = dict()
 p = sub.Popen(('sudo', 'tcpdump', '-i', 'eno2', '-s', '0', '-l', '-w', '-', 'dst', 'port 3306'), stdout=sub.PIPE)
 for row in iter(p.stdout.readline, b''):  
   if row.find('INSERT') > -1:
-    query_obj = re.search('(?<=INSERT INTO `)([a-z_0-9]*)`.`([a-z_0-9]*)', row)
-    query['dbname'] = query_obj.group(0)
-    query['tname'] = query_obj.group(1)
+    row_substr = re.search('(?<=INSERT INTO `)([a-z_0-9]*)`.`([a-z_0-9]*)', row)
+    query['dbname'] = row_substr.group(0)
+    query['tname'] = row_substr.group(1)
     query['type'] = 'INSERT'
     print query
   else:
