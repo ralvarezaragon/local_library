e4266a002474903327504135f7a8361df4a4141c
ralvarezaragon <rafael.alvarez@basebone.com>
2017-08-30 12:06:39 +0200

    dev updates

--- python/prom_exporters/profile_exporter.py
+++ python/prom_exporters/profile_exporter.py
@@ -6,5 +6,5 @@ import re
 
 p = sub.Popen(('sudo', 'tcpdump', '-i', 'eno2', '-s', '0', '-l', '-w', '-', 'dst', 'port 3306'), stdout=sub.PIPE)
 for row in iter(p.stdout.readline, b''):  
-  if row.find('INSERT', beg=0, end=len(row) > -1):
+  if row.find('INSERT') > -1:
     print ("insert counted")
