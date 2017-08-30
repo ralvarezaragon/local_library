566c14fef76dccc5c20faf61e82d7fb2ffdf627d
ralvarezaragon <rafael.alvarez@basebone.com>
2017-08-30 11:17:06 +0200

    dev updates

--- python/prom_exporters/profile_exporter.py
+++ python/prom_exporters/profile_exporter.py
@@ -2,7 +2,7 @@
 
 import pyshark
 
-capture = pyshark.LiveCapture(interface='eno2')
+capture = pyshark.LiveCapture(interface='rpcapd://10.0.3.53:3306/eno2')
 capture.sniff(timeout=50)
 
 for packet in capture.sniff_continuously(packet_count=5):
