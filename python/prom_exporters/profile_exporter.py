d1fc95ea1b95a071854d5c0293592db9776b95a7
ralvarezaragon <rafael.alvarez@basebone.com>
2017-08-30 12:04:38 +0200

    dev updates

--- python/prom_exporters/profile_exporter.py
+++ python/prom_exporters/profile_exporter.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 
 import subprocess as sub
+import re
+
 
 p = sub.Popen(('sudo', 'tcpdump', '-i', 'eno2', '-s', '0', '-l', '-w', '-', 'dst', 'port 3306'), stdout=sub.PIPE)
 for row in iter(p.stdout.readline, b''):  
-    print row.rstrip()
-    print "=============================================="
+  if row.find('INSERT', beg=0, end=len(string) > -1):
+    print ("insert counted")
