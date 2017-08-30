227579bd7e2481874ee75f73aaeecdb6621aac95
ralvarezaragon <rafael.alvarez@basebone.com>
2017-08-30 12:13:53 +0200

    dev updates

--- python/prom_exporters/profile_exporter.py
+++ python/prom_exporters/profile_exporter.py
@@ -7,4 +7,10 @@ import re
 p = sub.Popen(('sudo', 'tcpdump', '-i', 'eno2', '-s', '0', '-l', '-w', '-', 'dst', 'port 3306'), stdout=sub.PIPE)
 for row in iter(p.stdout.readline, b''):  
   if row.find('INSERT') > -1:
-    print ("insert counted")
+    query_obj = re.search('(?<=INSERT INTO `)([a-z_0-9]*)`.`([a-z_0-9]*)', query)
+    query['dbname'] = dbname_obj.group(0)
+    query['tname'] = dbname_obj.group(1)
+    query['type'] = 'INSERT'
+    print query
+  else:
+    print row.rstrip()
