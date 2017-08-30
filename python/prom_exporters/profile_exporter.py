1b90a0a5304bc06ca505aed4069e40537068cd29
ralvarezaragon <rafael.alvarez@basebone.com>
2017-08-30 12:24:06 +0200

    dev updates

--- python/prom_exporters/profile_exporter.py
+++ python/prom_exporters/profile_exporter.py
@@ -8,14 +8,14 @@ p = sub.Popen(('sudo', 'tcpdump', '-i', 'eno2', '-s', '0', '-l', '-w', '-', 'dst
 for row in iter(p.stdout.readline, b''):  
   if row.find('INSERT') > -1:
     row_substr = re.search('(?<=INSERT INTO `)([a-z_0-9]*)`.`([a-z_0-9]*)', row)
-    query['dbname'] = row_substr.group(2)
-    query['tname'] = row_substr.group(3)
+    query['dbname'] = row_substr.group(1)
+    query['tname'] = row_substr.group(2)
     query['type'] = 'INSERT'
    
   elif row.find('SELECT') > -1:
     row_substr = re.search('(?<=SELECT .* FROM `)([a-z_0-9]*)`.`([a-z_0-9]*)', row)
-    query['dbname'] = row_substr.group(2)
-    query['tname'] = row_substr.group(3)
+    query['dbname'] = row_substr.group(1)
+    query['tname'] = row_substr.group(2)
     query['type'] = 'SELECT'
   else:
     print row.rstrip()
