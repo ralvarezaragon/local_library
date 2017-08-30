52c4179507814f34536b07dcc8cec92461e78b50
ralvarezaragon <rafael.alvarez@basebone.com>
2017-08-30 12:33:51 +0200

    dev updates

--- python/prom_exporters/profile_exporter.py
+++ python/prom_exporters/profile_exporter.py
@@ -3,21 +3,23 @@
 import subprocess as sub
 import re
 
-query = dict()
+
 p = sub.Popen(('sudo', 'tcpdump', '-i', 'eno2', '-s', '0', '-l', '-w', '-', 'dst', 'port 3306'), stdout=sub.PIPE)
-for row in iter(p.stdout.readline, b''):  
+for row in iter(p.stdout.readline, b''):
+  query = dict()
   if row.find('INSERT') > -1:
     row_substr = re.search('(INSERT INTO `)([a-z_0-9]*)`.`([a-z_0-9]*)', row)
     query['dbname'] = row_substr.group(2)
     query['tname'] = row_substr.group(3)
-    query['type'] = 'INSERT'
-   
-  elif row.find('SELECT *') > -1:
-    print row.rstrip()
+    query['type'] = 'INSERT'   
+  elif row.find('SELECT ') > -1 and row.find('SELECT 1') == -1:    
     row_substr = re.search('(SELECT .* FROM `)([a-z_0-9]*)`.`([a-z_0-9]*)', row)
     query['dbname'] = row_substr.group(2)
     query['tname'] = row_substr.group(3)
     query['type'] = 'SELECT'
-    
+  elif row.find('UPDATE') > -1 or row.find('DELETE') > -1 or row.find('DROP') > -1:
+    query['dbname'] = 'NA'
+    query['tname'] = 'NA'
+    query['type'] = row.rstrip()
   print query
   print "==========================="
