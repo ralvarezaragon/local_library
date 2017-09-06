77dc9094409943d22e08c081084e6903d6ee0dc8
ralvarezaragon <rafael.alvarez@basebone.com>
2017-09-06 12:20:46 +0200

    blah

--- python/prom_exporters/profile_exporter.py
+++ python/prom_exporters/profile_exporter.py
@@ -45,27 +45,22 @@ for row in iter(p.stdout.readline, b''):
     try:
       print get_insert_row(row)
     except Exception as e:
-      print e
-      print row.rstrip()      
+      err = 1    
   elif row.find('SELECT ') > -1 and row.find(' FROM') > -1:
     try:
-      print get_select_row(row)
+      print get_select_row(row) 
     except Exception as e:
-      print e
-      print row.rstrip()      
+      err = 1 
   elif row.find('UPDATE') > -1 and row.find('SET') > -1:
     try:  
       print get_update_row(row)
     except Exception as e:
-      print e
-      print row.rstrip()
-      exit()
+      err = 1
   elif row.find('REPLACE INTO ') > -1:
     try:  
       print get_replace_row(row)
     except Exception as e:
-      print e
-      print row.rstrip()       
+      err = 1     
   elif (row.find('DELETE') > -1 and row.find(' FROM') > -1):
       print row.rstrip()
       exit()
