d757fb2d3cde0fd20bd932add1c09aadb9c083d9
ralvarezaragon <rafael.alvarez@basebone.com>
2017-09-15 10:52:16 +0200

    pyspark testing

--- python/pyspark_s3_test.py
+++ python/pyspark_s3_test.py
@@ -3,29 +3,41 @@
 import argparse
 from pyspark import SparkContext, SparkConf
 from boto.s3.connection import S3Connection
-
+from boto3.session import Session
 
 def option_menu():
   parser = argparse.ArgumentParser()
   parser.add_argument(
     '--access_key', metavar='a', dest='access_key', type=str,
-    help="Select the target cluster [hopper|stats|frank]"
+    help="Identify yourself with the key"
   )
   parser.add_argument(
     '--secret_key', metavar='s', dest='secret_key', type=str, 
-    help="Declare the target database"
+    help="the secret for teh access key"
   )  
   args = parser.parse_args()
   return args
 
 opt = option_menu()
-conn = S3Connection(
-    opt.access_key,
-    opt.secret_key
+
+#conn = S3Connection(
+#    opt.access_key,
+#    opt.secret_key
+#)
+#bucket = conn.get_bucket('basebone.backups')
+#keys = bucket.list()
+#print keys
+
+session = Session(
+  aws_access_key_id=opt.access_key,
+  aws_secret_access_key=opt.secret_key
 )
-bucket = conn.get_bucket('basebone.backups')
-keys = bucket.list()
-print keys
+s3 = session.resource('s3')
+your_bucket = s3.Bucket('basebone.backups')
+
+for s3_file in your_bucket.objects.all():
+    print(s3_file.key)
+
   # Get a Spark context and use it to parallelize the keys
   #conf = SparkConf().setAppName("MyFileProcessingApp")
   #sc = SparkContext(conf=conf)
