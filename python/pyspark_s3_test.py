d8a9b571ab389bb77d3c6846d4b1c388b5466462
ralvarezaragon <rafael.alvarez@basebone.com>
2017-09-18 09:05:08 +0200

    spark test

--- python/pyspark_s3_test.py
+++ python/pyspark_s3_test.py
@@ -18,7 +18,7 @@ l_key = []
 # List the files within the desired folder for the given bucket
 for s3_file in bucket.objects.filter(Prefix='test_log'):
   l_key.append(s3_file.key)
-  print "New file added: {0}".format(s3_file.key)
+  print "  >>> New file added: {0}".format(s3_file.key)
  
 # Get a Spark context and use it to parallelize the keys
 conf = SparkConf().setAppName("apptest1")
@@ -26,4 +26,4 @@ sc = SparkContext(conf=conf)
 
 sc_key = sc.parallelize(l_key)
 rdd = sc_key.flatMap(distributed_file_read)
-print "Count of row: {0}".format(rdd.count)
+print "  >>> Count of row: {0}".format(rdd.count())
