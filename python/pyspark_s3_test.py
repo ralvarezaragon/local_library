86878732daef7c73c64ec00d26769498cdc42b99
ralvarezaragon <rafael.alvarez@basebone.com>
2017-09-18 09:42:20 +0200

    spark test

--- python/pyspark_s3_test.py
+++ python/pyspark_s3_test.py
@@ -8,6 +8,7 @@ import boto3
 def distributed_file_read(file_key):
     s3_obj = boto3.resource('s3').Object(bucket_name='basebone.backups', key=file_key)
     body = s3_obj.get()['Body'].read()
+    # Split the body by lines so now we ahve a list of elements
     res = body.splitlines()
     return res
         
@@ -22,11 +23,11 @@ for s3_file in bucket.objects.filter(Prefix='test_log'):
   print "  >>> New file added: {0}".format(s3_file.key)
  
 # Get a Spark context and use it to parallelize the keys
-#conf = SparkConf().setAppName("apptest1")
-#sc = SparkContext(conf=conf)
-#sc_key = sc.parallelize(l_key)
-#rdd = sc_key.flatMap(distributed_file_read)
-#print "  >>> Count of row: {0}".format(rdd.count())
-#print rdd.collect()
+conf = SparkConf().setAppName("apptest1")
+sc = SparkContext(conf=conf)
+sc_key = sc.parallelize(l_key)
+rdd = sc_key.flatMap(distributed_file_read)
+print "  >>> Count of row: {0}".format(rdd.count())
+print rdd.collect()
 
-print distributed_file_read(l_key[0])
+#print distributed_file_read(l_key[0])
