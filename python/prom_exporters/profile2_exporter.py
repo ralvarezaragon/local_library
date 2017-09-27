374ac5190bb181edfe02b2bca8a9b74778b3cca8
ralvarezaragon <rafael.alvarez@basebone.com>
2017-09-27 14:28:38 +0200

    new profiling aproach

--- python/prom_exporters/profile2_exporter.py
+++ python/prom_exporters/profile2_exporter.py
@@ -35,12 +35,11 @@ p = sub.Popen(('sudo', 'tcpdump', '-i', 'em2', '-s', '0', '-l', '-w', '-'), stdo
 for line in iter(p.stdout.readline, b''):
   if line.find('MYSQL_PHP') > -1 or line.find('MYSQL_JAVA') > -1:
     query = dict()
-    row_substr = re.search('^(.*) (\w+) (\w+). (\w+) (\w+). (\S+) (\w+) (\w+) (.*\*\/) (\S*)', line)
+    row_substr = re.search('^(.*) (\w+) (\w+). (\w+) (\w+). (\S+) (\w+) (\w+) (.*\*\/) (\S*)', line)      
     try:
-      ts = row_substr.group(1)
+      query['source'] = row_substr.group(4)
     except Exception as e:
-      ts = ''
-    query['source'] = row_substr.group(4)
+      query['source'] = ''
     try:
       query['sender'] = row_substr.group(2)
     except Exception as e:
