2dbd4497bee31303edea05d0f9b921179664e255
ralvarezaragon <rafael.alvarez@basebone.com>
2017-09-27 09:10:55 +0200

    query hash added

--- sql/table_sizes_list.sql
+++ sql/table_sizes_list.sql
@@ -2,5 +2,6 @@ SELECT table_schema,
 table_name,
 round(((data_length + index_length) / 1024 / 1024),2) 'Size in MB'
 FROM information_schema.TABLES
-WHERE TABLE_TYPE='BASE TABLE';
+WHERE TABLE_TYPE='BASE TABLE'
+AND table_name = 'passthrough_tracking';
 
