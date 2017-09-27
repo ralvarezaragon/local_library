5586bf0a4a5a0a6f2730eb2dc7ecbd09e46502d7
ralvarezaragon <rafael.alvarez@basebone.com>
2017-09-27 14:43:45 +0200

    new profiling aproach

--- python/prom_exporters/profile2_exporter.py
+++ python/prom_exporters/profile2_exporter.py
@@ -28,51 +28,53 @@ def parse_ip(par):
 
 
 start_http_server(8005)
-c = Counter('mysql_profile2', 'Mysql profiling metrics from PHP logs', ['source', 'target', 'dbname', 'module', 'query_type'])
+c = Counter('mysql_profile2', 'Mysql profiling metrics from PHP logs', ['sender', 'source', 'target', 'dbname', 'module', 'hash', 'query_type'])
 
 p = sub.Popen(('sudo', 'tcpdump', '-i', 'em2', '-s', '0', '-l', '-w', '-'), stdout=sub.PIPE)
 
 for line in iter(p.stdout.readline, b''):
   if line.find('MYSQL_PHP') > -1 or line.find('MYSQL_JAVA') > -1:
     query = dict()
-    row_substr = re.search('^(.*) (\w+) (\w+). (\w+) (\w+). (\S+) (\w+) (\w+) (.*\*\/) (\S*)', line)      
+    row_substr = re.search('^(.*) (\w+) (\w+). (\w+) (\w+). (\S+) (\w+) (\w+) (.*\*\/) (\S*)', line)
     try:
       query['source'] = row_substr.group(4)
     except Exception as e:
-      query['source'] = ''
+      err = 1
     try:
       query['sender'] = row_substr.group(2)
     except Exception as e:
-      query['sender'] = ''
+      err = 1
     try:
       query['module'] = row_substr.group(5)
     except Exception as e:
-      query['module'] = ''
+      err = 1
     try:
       query['target'] = parse_ip(row_substr.group(6))
     except Exception as e:
-      query['target'] = ''
+      err = 1
     try:
       query['dbname'] = row_substr.group(7)
     except Exception as e:
-      query['dbname'] = ''
+      err = 1
     try:
       query['hash'] = row_substr.group(8)
     except Exception as e:
-      query['hash'] = ''
+      err = 1
     try:
       query['type'] = row_substr.group(10)
     except Exception as e:
-      query['type'] = line
+      err = 1
     print query
-    #print line
+    c.labels(
+      sender = query['sender'],
+      source = query['source'],
+      target = query['target'],
+      dbname = query['dbname'],
+      module = query['module'],
+      hash = query['hash'],
+      query_type = query['type']
+    ).inc()
 
 
 
-#c.labels(
-#     source=query['source'],
-#      target=query['target'],
-#      dbname=query['dbname'],
-#      module=query['module'],
-#      query_type=query['type']
-#    ).inc()
+
