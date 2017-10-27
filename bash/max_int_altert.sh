0f5c7afb3266922a8f37fc59931206eec4ef95ed
ralvarezaragon <rafael.alvarez@basebone.com>
2017-10-27 08:50:04 +0200

    regular update

--- bash/max_int_altert.sh
+++ bash/max_int_altert.sh
@@ -37,8 +37,8 @@ WHERE
 #while read line; do
 #    result_a+=("$line")
 #done < <(mysql -h ${host} -e "${sql}")
-
-read -ra vars <<< $(mysql -h ${host} -u ro -pinyourhonorbestofyou -e ${sql})
-for i in "${vars[@]}"; do
-    echo $i
-done
+mysql -h ${host} -u ro -pinyourhonorbestofyou -e ${sql}
+#read -ra vars <<< $()
+#for i in "${vars[@]}"; do
+#    echo $i
+#done
