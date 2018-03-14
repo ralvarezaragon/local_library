029944f4014bb627d392082108ebdd1724301ca1
ralvarez <rafael.alvarez@basebone.com>
2018-03-14 16:59:50 +0100

    regular update

--- sql/partitions_info.sql
+++ sql/partitions_info.sql
@@ -1,6 +1,28 @@
 SELECT TABLE_SCHEMA, TABLE_NAME, partition_name, PARTITION_ORDINAL_POSITION, TABLE_ROWS, PARTITION_METHOD
 FROM information_schema.PARTITIONS
-WHERE PARTITION_ORDINAL_POSITION IS NOT NULL;
--- WHERE TABLE_SCHEMA = 'ads_pixel' AND TABLE_NAME = 'pixel_registry';
+WHERE TABLE_SCHEMA = 'ads_log' AND TABLE_NAME = 'ads_tracking';
 
 -- ALTER TABLE ads_pixel.pixel_registry TRUNCATE PARTITION p9;
+
+
+SELECT TABLE_SCHEMA, TABLE_NAME
+FROM information_schema.PARTITIONS
+where partition_name is not null
+group by TABLE_SCHEMA, TABLE_NAME
+
+
+admin@10.0.8.51 [(none)]> SELECT TABLE_SCHEMA, TABLE_NAME
+    -> FROM information_schema.PARTITIONS
+    -> where partition_name is not null
+    -> group by TABLE_SCHEMA, TABLE_NAME;
++----------------------------+----------------------------+
+| TABLE_SCHEMA               | TABLE_NAME                 |
++----------------------------+----------------------------+
+| esme_billing               | threshold_reminder         |
+| esme_limits                | subscription_billing_cache |
+| esme_messaging             | customer_activity          |
+| esme_oneoff                | mpesa_ke_purchases         |
+| esme_reminders             | latest_reminder            |
+| esme_reminders             | reminder_completed         |
+| esme_subscriptions_history | stop_confirmation          |
++----------------------------+----------------------------+
